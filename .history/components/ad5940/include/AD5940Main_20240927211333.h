c66dd36bbdb9103267ef459a40f552edf3931f26
HsingyouZhou <1044358896@qq.com>
2024-09-27 23:06:36 +0800

    New repo

---
+++ .history/components/ad5940/include/AD5940Main_20240927211333.h
@@ -0,0 +1,6 @@
+#ifndef _AD5940MAIN_H_
+#define _AD5940MAIN_H_
+
+void AD5940_Main(void);
+
+#endif
---
+++ .history/main/main_20240927213606.c
@@ -0,0 +1,29 @@
+#include "ad5940.h"
+#include "AD5940Main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "string.h"
+
+// freeRTOS related includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// driver related includes
+#include "driver/uart.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
+
+
+void app_main() 
+{
+  printf("OK");
+  AD5940_Main();
+}
+
